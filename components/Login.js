fb35b8d011b2d2c6965131829a6a50991cc287ae
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-05 20:52:56 +0530

    Add input fields and button to the login component

--- components/Login.js
+++ components/Login.js
@@ -6,8 +6,28 @@ import { useNavigation } from '@react-navigation/native';
 export default function Login(){
 return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text variant="titleLarge">STUDENT LOGIN</Text>
+    <StatusBar style="auto" />
+    <Divider/>
+  
+    <TextInput
+       mode="flat"
+       label="Username"
+       value={username}
+       onChangeText={setUsername}
+       style={styles.input}
+      />
+      <TextInput
+       mode="flat"
+       label="Password"
+       value={password}
+       onChangeText={setPassword}
+       style={styles.input}
+      />
+      <Button mode="contained" onPress={pressed}>
+       Login
+      </Button>
+
     </View>
   );
 }
@@ -19,4 +39,23 @@ export const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 8,
+      },
+      input: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingLeft: 8,
+        marginBottom: 16, // Creates space between input fields and button
+      },
+      inputWhite: {
+        backgroundColor: 'white', // White background
+        color: 'black', // Black font color
+      },
+      button: {
+        marginTop: 20, // Adds space above the button
+      },
   });
