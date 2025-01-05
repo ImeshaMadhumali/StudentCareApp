3135416248915b08eaf4ffda1c33230f11632f38
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-05 19:30:27 +0530

    Add SafeAreaProvider to the App

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider, SafeAreaProvider } from 'react-native-paper';
-
+import { PaperProvider } from 'react-native-paper';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
