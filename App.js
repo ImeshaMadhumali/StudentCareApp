4b817ae3f2ee62020e7d68e19dea56df97bbb6c1
Imesha Madhumali <imeshamadhumali31@gmail.com>
2025-01-05 19:51:18 +0530

    Add required images to the assets folder

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { PaperProvider } from 'react-native-paper';
+import { Divider, PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+//import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <PaperProvider>
       <SafeAreaProvider>
         <View style={styles.container}>
+        <Text variant="titleLarge">STUDENT LOGIN</Text>
+        <Divider/>
           <Text>Open up App.js to start working on your app!</Text>
           <StatusBar style="auto" />
         </View>
